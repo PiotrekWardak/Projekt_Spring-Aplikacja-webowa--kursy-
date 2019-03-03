f31ab4adaf0499f893b44a582ea9faf5e7ac2ef3
PiotrWardak <piotrekwardak@gmail.com>
2019-03-03 08:20:46 +0100

    2DzienZajecDodatkowePliki

---
+++ src/main/java/com/bootcamp/bootcamp/model/Trainer.java
@@ -0,0 +1,50 @@
+package com.bootcamp.bootcamp.model;
+
+public class Trainer {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
